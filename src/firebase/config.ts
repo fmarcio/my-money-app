82e9a3a1954b7261b6400456e3c56488ab6cea85
Marcio Fonseca <marcio.fonseca88@gmail.com>
2026-05-05 20:26:02 -0300

    security: move firebase config to environment variables and update gitignore

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,12 +3,12 @@ import { getFirestore, Timestamp } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAg-4K8DnojkkYRO48wmE8tB0a_kJgWKd8",
-  authDomain: "mymoney-ee086.firebaseapp.com",
-  projectId: "mymoney-ee086",
-  storageBucket: "mymoney-ee086.appspot.com",
-  messagingSenderId: "588317924664",
-  appId: "1:588317924664:web:b8cca927f1b73bdfcef5cd",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
